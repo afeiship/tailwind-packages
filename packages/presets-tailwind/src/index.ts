f2028ac7ba4029f048b29d1055fbd44da160840b
afeiship <1290657123@qq.com>
2024-04-16 21:43:42 +0800

    feat: add aric presets

--- packages/presets-tailwind/src/index.ts
+++ packages/presets-tailwind/src/index.ts
@@ -1,6 +1,11 @@
 const nxPresets = () => {
   return {
-    plugins: [require('@jswork/tailwind-lc'), require('@jswork/tailwind-width')],
+    plugins: [
+      require('@jswork/tailwind-base'),
+      require('@jswork/tailwind-lc'),
+      require('@jswork/tailwind-width'),
+      require('@jswork/tailwind-font-size'),
+    ],
   };
 };
 
