42440917d118780989073d4d4570637932d97329
afeiship <1290657123@qq.com>
2024-04-21 11:30:47 +0800

    feat: add shortcuts

--- packages/presets-tailwind/src/index.ts
+++ packages/presets-tailwind/src/index.ts
@@ -2,9 +2,10 @@ const nxPresets = () => {
   return {
     plugins: [
       require('@jswork/tailwind-base'),
-      require('@jswork/tailwind-lc'),
-      require('@jswork/tailwind-width'),
       require('@jswork/tailwind-font-size'),
+      require('@jswork/tailwind-lc'),
+      require('@jswork/tailwind-shortcuts'),
+      require('@jswork/tailwind-width'),
     ],
   };
 };
