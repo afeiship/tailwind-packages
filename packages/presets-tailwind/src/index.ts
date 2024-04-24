584e569613bd51bb2d39a905ea24b87b11bbb42d
afeiship <1290657123@qq.com>
2024-04-24 09:59:07 +0800

    feat: add layouts

--- packages/presets-tailwind/src/index.ts
+++ packages/presets-tailwind/src/index.ts
@@ -3,6 +3,7 @@ const nxPresets = () => {
     plugins: [
       require('@jswork/tailwind-font-size'),
       require('@jswork/tailwind-heading'),
+      require('@jswork/tailwind-layouts'),
       require('@jswork/tailwind-lc'),
       require('@jswork/tailwind-shortcuts'),
       require('@jswork/tailwind-width')
