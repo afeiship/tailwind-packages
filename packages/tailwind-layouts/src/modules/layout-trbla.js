7e4b0e35d48f75d6eb4439265463dc04e85d200b
afeiship <1290657123@qq.com>
2024-04-28 20:41:10 +0800

    feat: add flex-1

--- packages/tailwind-layouts/src/modules/layout-trbla.js
+++ packages/tailwind-layouts/src/modules/layout-trbla.js
@@ -16,10 +16,14 @@ module.exports = function (pluginApi) {
   const { addComponents } = pluginApi
 
   return addComponents({
+    // class=^.layout-trbla-.*$
     '.layout-trbla-sa': {
       '& > *': {
         '&:first-child': {
           'flex-shrink': 0,
+        },
+        '&:nth-child(2)': {
+          flex: 1,
         },
       },
     },
@@ -32,12 +36,18 @@ module.exports = function (pluginApi) {
         '&:last-child': {
           'flex-shrink': 0,
         },
+        '&:nth-child(2)': {
+          flex: 1,
+        },
       },
     },
     '.layout-trbla-ae': {
       '& > *': {
         '&:last-child': {
           'flex-shrink': 0,
+        },
+        '&:nth-child(1)': {
+          flex: 1,
         },
       },
     },
