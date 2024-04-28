5da5ce869966f89ebbe71d27f62b6a8b5932d878
afeiship <1290657123@qq.com>
2024-04-28 12:56:12 +0800

    feat: add flex-shrint for trbla

--- packages/tailwind-layouts/src/modules/layout-trbla.js
+++ packages/tailwind-layouts/src/modules/layout-trbla.js
@@ -11,38 +11,50 @@
 // 'ab',
 // 'tb'
 
-module.exports = function(pluginApi) {
-  const { addComponents } = pluginApi;
+module.exports = function (pluginApi) {
+  const { addComponents } = pluginApi
 
   addComponents({
     // class=^.layout-trbla-.*$
     '[class^="layout-trbla-"]': {
-      display: 'flex'
+      display: 'flex',
     },
     '.layout-trbla-sa': {
       '& > *': {
+        '&:first-child': {
+          'flex-shrink': 0,
+        },
         '&:nth-child(2)': {
-          flex: 1
-        }
-      }
+          flex: 1,
+        },
+      },
     },
     '.layout-trbla-sae': {
       justifyContent: 'space-between',
       '& > *': {
+        '&:first-child': {
+          'flex-shrink': 0,
+        },
+        '&:last-child': {
+          'flex-shrink': 0,
+        },
         '&:nth-child(2)': {
-          flex: 1
-        }
-      }
+          flex: 1,
+        },
+      },
     },
     '.layout-trbla-ae': {
       '& > *': {
+        '&:last-child': {
+          'flex-shrink': 0,
+        },
         '&:nth-child(1)': {
-          flex: 1
-        }
-      }
+          flex: 1,
+        },
+      },
     },
     '.layout-trbla-se': {
-      justifyContent: 'space-between'
-    }
-  });
-};
+      justifyContent: 'space-between',
+    },
+  })
+}
