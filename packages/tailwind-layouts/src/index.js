3998e5ab5b3ed8115f1398c2da9709e22f7a708e
afeiship <1290657123@qq.com>
2024-05-09 07:17:54 +0800

    feat: add layout inline itmes

--- packages/tailwind-layouts/src/index.js
+++ packages/tailwind-layouts/src/index.js
@@ -1,6 +1,7 @@
 const plugin = require('tailwindcss/plugin')
 const moduleLayoutAbsCenter = require('./modules/layout-abs-center')
 const moduleLayoutAbsScrollable = require('./modules/layout-abs-scrollable')
+const moduleLayoutInlineItems = require('./modules/layout-inline-items')
 const moduleLayoutStickyFooter = require('./modules/layout-sticky-footer')
 const moduleTransCenter = require('./modules/layout-trans-center')
 const moduleLayoutTrbla = require('./modules/layout-trbla')
@@ -9,6 +10,7 @@ module.exports = plugin(function (pluginApi) {
   // Add custom utilities here
   moduleLayoutAbsCenter(pluginApi)
   moduleLayoutAbsScrollable(pluginApi)
+  moduleLayoutInlineItems(pluginApi)
   moduleLayoutStickyFooter(pluginApi)
   moduleTransCenter(pluginApi)
   moduleLayoutTrbla(pluginApi)
