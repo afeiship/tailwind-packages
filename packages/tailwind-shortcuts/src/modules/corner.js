74ea172e5d0b9353ef8ce62455f810ec8b11f685
afeiship <1290657123@qq.com>
2024-05-14 22:25:44 +0800

    feat: add corner prefix

--- packages/tailwind-shortcuts/src/modules/corner.js
+++ packages/tailwind-shortcuts/src/modules/corner.js
@@ -1,8 +1,11 @@
 // This file is used to add the corner classes to the tailwind config.
 
 module.exports = function (pluginConfig) {
-  const { addComponents } = pluginConfig
+  const { addComponents } = pluginConfig;
   return addComponents({
+    '[class*="corner-"]': {
+      position: 'absolute',
+    },
     '.corner-top-left, .corner-t': {
       top: 0,
       left: 0,
@@ -18,5 +21,5 @@ module.exports = function (pluginConfig) {
       bottom: 0,
       left: 0,
     },
-  })
-}
+  });
+};
