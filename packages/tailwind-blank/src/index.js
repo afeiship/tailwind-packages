28780f9f09e52257728493013d8aa60459b54881
afeiship <1290657123@qq.com>
2024-05-17 13:56:02 +0800

    feat: add blank-x blank-y

--- packages/tailwind-blank/src/index.js
+++ packages/tailwind-blank/src/index.js
@@ -4,11 +4,16 @@ module.exports = plugin(
   function({ matchUtilities, theme }) {
     matchUtilities(
       {
-        blank: (value) => ({
-          height: value,
+        '[class*="blank-"]': {
           fontSize: 0,
           lineHeight: 0,
-          clean: 'both'
+          clean: 'both',
+        },
+        '.blank-x': (value) => ({
+          width: value
+        }),
+        '.blank-y': (value) => ({
+          height: value
         })
       },
       { values: theme('spacing') }
