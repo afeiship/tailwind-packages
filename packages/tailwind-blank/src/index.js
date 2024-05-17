f600105d908c7ce9308f51c9430866ed6b7cf3a6
afeiship <1290657123@qq.com>
2024-05-17 15:25:36 +0800

    feat: add missing builder

--- packages/tailwind-blank/src/index.js
+++ packages/tailwind-blank/src/index.js
@@ -1,5 +1,6 @@
 const plugin = require('tailwindcss/plugin');
-
+// This plugin will add a new class called "blank-x" and "blank-y" to your HTML elements.
+// You can use these classes to create a blank space of a certain width or height.
 module.exports = plugin(
   function({ matchUtilities, theme }) {
     matchUtilities(
