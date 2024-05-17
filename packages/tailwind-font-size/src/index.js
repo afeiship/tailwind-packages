dcc4deae4d39cb48c70bde552fd178ac53f75b24
afeiship <1290657123@qq.com>
2024-05-17 18:26:33 +0800

    fix: use rem 16

--- packages/tailwind-font-size/src/index.js
+++ packages/tailwind-font-size/src/index.js
@@ -2,13 +2,13 @@ const plugin = require('tailwindcss/plugin');
 
 module.exports = plugin(
   function({ addUtilities, theme }) {
-    const remBaseSize = theme('fontSize.base') || 16;
+    // const remBaseSize = theme('fontSize.base') || 16;
     const separator = theme('separator') || '-';
     addUtilities(
       (() => {
         let utilities = {};
         Array.from({ length: 100 }, (_, i) => i + 1).forEach((i) => {
-          utilities[`.f${separator}${i}`] = { fontSize: `${i / remBaseSize}rem` };
+          utilities[`.f${separator}${i}`] = { fontSize: `${i / 16}rem` };
         });
         return utilities;
       })(),
