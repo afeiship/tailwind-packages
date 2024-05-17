b35e48eb8fc562dcfad1951b565545bba62bfa0e
afeiship <1290657123@qq.com>
2024-05-17 16:40:47 +0800

    feat: add debug outline red

--- packages/tailwind-shortcuts/src/modules/debug.js
+++ packages/tailwind-shortcuts/src/modules/debug.js
@@ -1,14 +1,24 @@
-module.exports = function (pluginConfig) {
-  const { addComponents } = pluginConfig
+module.exports = function(pluginConfig) {
+  const { addComponents } = pluginConfig;
   return addComponents({
     '.debug-red': {
-      border: '1px solid #f00',
+      border: '1px solid #f00'
     },
     '.debug-green': {
-      border: '1px solid #0f0',
+      border: '1px solid #0f0'
     },
     '.debug-blue': {
-      border: '1px solid #00f',
+      border: '1px solid #00f'
     },
-  })
-}
+    // use outline
+    '.debug-outline-red': {
+      outline: '1px solid #f00'
+    },
+    '.debug-outline-green': {
+      outline: '1px solid #0f0'
+    },
+    '.debug-outline-blue': {
+      outline: '1px solid #00f'
+    }
+  });
+};
