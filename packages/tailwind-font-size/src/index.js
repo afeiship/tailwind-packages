2b9288c5dfab75af841a7e134464245f3e08489d
afeiship <1290657123@qq.com>
2024-05-17 18:21:32 +0800

    feat: fz -> f-

--- packages/tailwind-font-size/src/index.js
+++ packages/tailwind-font-size/src/index.js
@@ -3,11 +3,12 @@ const plugin = require('tailwindcss/plugin');
 module.exports = plugin(
   function({ addUtilities, theme }) {
     const remBaseSize = theme('fontSize.base') || 16;
+    const separator = theme('separator') || '-';
     addUtilities(
       (() => {
         let utilities = {};
         Array.from({ length: 100 }, (_, i) => i + 1).forEach((i) => {
-          utilities[`.f${i}`] = { fontSize: `${i / remBaseSize}rem` };
+          utilities[`.f${separator}${i}`] = { fontSize: `${i / remBaseSize}rem` };
         });
         return utilities;
       })(),
