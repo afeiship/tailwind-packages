5fd7810007b367764b3e314cd027d655a49a22f5
afeiship <1290657123@qq.com>
2024-05-17 18:09:06 +0800

    fix: upt 0.25 rem

--- packages/tailwind-font-size/src/index.js
+++ packages/tailwind-font-size/src/index.js
@@ -1,4 +1,4 @@
-const plugin = require('tailwindcss/plugin')
+const plugin = require('tailwindcss/plugin');
 
 module.exports = plugin(
   function({ addUtilities }) {
@@ -6,11 +6,11 @@ module.exports = plugin(
       (() => {
         let utilities = {};
         Array.from({ length: 100 }, (_, i) => i + 1).forEach((i) => {
-          utilities[`.f${i}`] = { fontSize: `${i} * 0.25rem` };
+          utilities[`.f${i}`] = { fontSize: `${i * 0.25}rem` };
         });
         return utilities;
       })(),
       ['responsive']
-    )
+    );
   }
-)
+);
