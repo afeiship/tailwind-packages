b7892bb0db62081c4aee8d27049a807c6d67c58a
afeiship <1290657123@qq.com>
2024-05-18 14:48:36 +0800

    feat: add blanks

--- packages/tailwind-blank/src/index.js
+++ packages/tailwind-blank/src/index.js
@@ -1,23 +1,46 @@
 const plugin = require('tailwindcss/plugin');
-// This plugin will add a new class called "blank-x" and "blank-y" to your HTML elements.
-// You can use these classes to create a blank space of a certain width or height.
-module.exports = plugin(
-  function({ matchUtilities, theme }) {
-    matchUtilities(
-      {
-        '[class*="blank-"]': {
-          fontSize: 0,
-          lineHeight: 0,
-          clean: 'both'
-        },
-        'blank-x': (value) => ({
-          width: value
-        }),
-        'blank-y': (value) => ({
-          height: value
-        })
+const generateBlanks = (value, callback) => {
+  const pairs = value.split(',');
+  const result = {};
+  pairs.forEach((pair, index) => {
+    result[`> *:nth-child(` + (index + 1) + ')'] = callback(pair, index);
+  });
+  return result;
+};
+
+module.exports = plugin(function ({ matchUtilities, theme }) {
+  matchUtilities(
+    {
+      '[class*="blank-"]': {
+        fontSize: 0,
+        lineHeight: 0,
+        clean: 'both',
       },
-      { values: theme('spacing') }
-    );
-  }
-);
+      'blank-x': (value) => ({
+        width: value,
+      }),
+      'blank-y': (value) => ({
+        height: value,
+      }),
+    },
+    { values: theme('spacing') },
+  );
+
+  matchUtilities(
+    {
+      'blanks-r': (value) => {
+        return generateBlanks(value, (pair) => ({ marginRight: `${pair * 0.25}rem` }));
+      },
+      'blanks-l': (value) => {
+        return generateBlanks(value, (pair) => ({ marginLeft: `${pair * 0.25}rem` }));
+      },
+      'blanks-t': (value) => {
+        return generateBlanks(value, (pair) => ({ marginTop: `${pair * 0.25}rem` }));
+      },
+      'blanks-b': (value) => {
+        return generateBlanks(value, (pair) => ({ marginBottom: `${pair * 0.25}rem` }));
+      },
+    },
+    { values: theme('spacing') },
+  );
+});
