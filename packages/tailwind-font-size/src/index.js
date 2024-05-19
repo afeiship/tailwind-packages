4835d17a6f1c4e819fcfa0eaea03e0b4d9460c65
afeiship <1290657123@qq.com>
2024-05-19 14:55:14 +0800

    refactor: use f/f-px prefix

--- packages/tailwind-font-size/src/index.js
+++ packages/tailwind-font-size/src/index.js
@@ -1,15 +1,15 @@
 const plugin = require('tailwindcss/plugin')
 
 module.exports = plugin(function ({ addUtilities, theme }) {
-  addUtilities(
-    (() => {
-      let utilities = {}
-      Array.from({ length: 100 }, (_, i) => i + 1).forEach((i) => {
-        utilities[`.f-${i}`] = { fontSize: `${i * 0.25}rem` }
-        utilities[`.f-px-${i}`] = { fontSize: `${i / 16}rem` }
-      })
-      return utilities
-    })(),
-    ['responsive']
+  matchUtilities(
+    {
+      f: (value) => ({
+        fontSize: value,
+      }),
+      'f-px': (value) => ({
+        fontSize: parseInt(value) / 16 + 'px',
+      }),
+    },
+    { values: theme('spacing') }
   )
 })
