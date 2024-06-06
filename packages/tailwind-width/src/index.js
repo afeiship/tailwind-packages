d8d11c07452227cfeade7f794aad9a5b8c281c87
afeiship <1290657123@qq.com>
2024-06-06 17:04:54 +0800

    feat: add min-max w1/wp tools

--- packages/tailwind-width/src/index.js
+++ packages/tailwind-width/src/index.js
@@ -1,30 +1,42 @@
-const plugin = require('tailwindcss/plugin');
+const plugin = require('tailwindcss/plugin')
 
 module.exports = plugin(
-  function({ matchUtilities, theme }) {
-    const values = theme('widths');
+  function ({ matchUtilities, theme }) {
+    const values = theme('widths')
 
     matchUtilities(
       {
         wp: (value) => ({
-          width: `${value * 10}%`
+          width: `${value * 10}%`,
         }),
         w1: (value) => ({
-          width: `${100 / value}%`
+          width: `${100 / value}%`,
         }),
         wh: (value) => {
-          const val = value.split(',');
-          const [width, height] = val.map((v) => `${v / 4}rem`);
-          return { width, height };
+          const val = value.split(',')
+          const [width, height] = val.map((v) => `${v / 4}rem`)
+          return { width, height }
         },
         'wh-px': (value) => {
-          const val = value.split(',');
-          const [width, height] = val.map((v) => `${v / 16}rem`);
-          return { width, height };
-        }
+          const val = value.split(',')
+          const [width, height] = val.map((v) => `${v / 16}rem`)
+          return { width, height }
+        },
+        'min-wp': (value) => ({
+          minWidth: `${value * 10}%`,
+        }),
+        'max-wp': (value) => ({
+          maxWidth: `${value * 10}%`,
+        }),
+        'min-w1': (value) => ({
+          minWidth: `${100 / value}%`,
+        }),
+        'max-w1': (value) => ({
+          maxWidth: `${100 / value}%`,
+        }),
       },
       { values }
-    );
+    )
   },
   {
     theme: {
@@ -38,8 +50,8 @@ module.exports = plugin(
         7: '7',
         8: '8',
         9: '9',
-        10: '10'
-      }
-    }
+        10: '10',
+      },
+    },
   }
-);
+)
