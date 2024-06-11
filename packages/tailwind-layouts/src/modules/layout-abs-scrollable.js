aaef09f4ed5b8f0f0cbb582ec3388a9fd1ebe516
afeiship <1290657123@qq.com>
2024-06-11 15:41:41 +0800

    feat: breack change for is-scrollable

--- packages/tailwind-layouts/src/modules/layout-abs-scrollable.js
+++ packages/tailwind-layouts/src/modules/layout-abs-scrollable.js
@@ -1,18 +1,28 @@
+const baseStyles = {
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  right: 0,
+  bottom: 0,
+  scrollBehavior: 'smooth',
+}
+
 module.exports = function (pluginApi) {
   const { addComponents } = pluginApi
+  addComponents('[class*="is-scrollable"]', baseStyles)
 
   return addComponents({
     '.layout-abs-scrollable': {
       position: 'relative',
       height: '100%',
-      '.is-scrollable': {
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        right: 0,
-        bottom: 0,
+      '.is-scrollable-xy': {
         overflow: 'auto',
-        scrollBehavior: 'smooth',
+      },
+      '.is-scrollable-x': {
+        overflowX: 'auto',
+      },
+      '.is-scrollable-y': {
+        overflowY: 'auto',
       },
     },
   })
