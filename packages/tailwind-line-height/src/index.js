395b10e0bfd9aa1f76301bf4ef6934e3df59ca6c
afeiship <1290657123@qq.com>
2024-10-10 09:37:00 +0800

    feat: add unitless/linep

--- packages/tailwind-line-height/src/index.js
+++ packages/tailwind-line-height/src/index.js
@@ -1,15 +1,23 @@
-const plugin = require('tailwindcss/plugin');
+const plugin = require('tailwindcss/plugin')
 
-module.exports = plugin(function({ matchUtilities, theme }) {
+module.exports = plugin(function ({ matchUtilities, theme }) {
   matchUtilities(
     {
       'lh': (value) => ({
-        lineHeight: value
+        lineHeight: value,
       }),
       'lh-px': (value) => ({
-        lineHeight: `${parseFloat(value) / 4}rem`
-      })
+        lineHeight: `${parseFloat(value) / 4}rem`,
+      }),
+      // percetage line height
+      'lhp': (value) => ({
+        lineHeight: `${value}%`,
+      }),
+      // number(unitless) line height
+      'lhn': (value) => ({
+        lineHeight: value,
+      }),
     },
     { values: theme('spacing') }
-  );
-});
+  )
+})
