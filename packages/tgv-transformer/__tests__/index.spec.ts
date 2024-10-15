6f8e52b6800d855a4d43d0bc0acf86ccfb21f0ee
afeiship <1290657123@qq.com>
2024-10-15 11:29:35 +0800

    wip

--- packages/tgv-transformer/__tests__/index.spec.ts
+++ packages/tgv-transformer/__tests__/index.spec.ts
@@ -4,5 +4,8 @@ describe('api.basic', () => {
   test('01 - Test tgv', () => {
     const result = tgv`sm:(debug-red bg-red-200) md:(debug-blue bg-blue-200) flex`;
     expect(result).toBe('sm:debug-red sm:bg-red-200 md:debug-blue md:bg-blue-200 flex');
+    const result2 = tgv`absolute desktop:(lh-5.5 right-15 bottom-3) mobile:(lh-3 right-5.5 bottom-1)`;
+    expect(result2).toBe('absolute desktop:lh-5.5 desktop:right-15 desktop:bottom-3 mobile:lh-3 mobile:right-5.5 mobile:bottom-1');
   });
+
 });
