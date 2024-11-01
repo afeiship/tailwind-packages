1431053693a0588378a45f554afec3c779adf1b6
afeiship <1290657123@qq.com>
2024-11-01 22:07:03 +0800

    fix: fw invalid when responsive

--- packages/tailwind-font-weight/src/index.js
+++ packages/tailwind-font-weight/src/index.js
@@ -1,13 +1,18 @@
 const plugin = require('tailwindcss/plugin');
 
 module.exports = plugin(function ({ addUtilities, theme }) {
-  addUtilities(() => {
-    let utilities = {};
-    for (let i = 1; i <= 9; i++) {
-      utilities[`.fw-${i}`] = {
-        fontWeight: i * 100
-      };
-    }
-    return utilities;
-  });
+  addUtilities(
+    () => {
+      let utilities = {};
+      for (let i = 1; i <= 9; i++) {
+        utilities[`.fw-${i}`] = {
+          fontWeight: i * 100,
+        };
+      }
+      return utilities;
+    },
+    {
+      variants: ['responsive', 'hover', 'focus', 'active', 'group-hover'],
+    },
+  );
 });
