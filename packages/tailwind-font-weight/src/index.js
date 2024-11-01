ce6ce35736a0a647e325e03d79636248e937a560
afeiship <1290657123@qq.com>
2024-11-01 23:16:26 +0800

    fix: fw num err

--- packages/tailwind-font-weight/src/index.js
+++ packages/tailwind-font-weight/src/index.js
@@ -1,22 +1,12 @@
 const plugin = require('tailwindcss/plugin');
 
-module.exports = plugin(
-  function ({ addUtilities, theme }) {
-    addUtilities(() => {
-      let utilities = {};
-      for (let i = 1; i <= 9; i++) {
-        utilities[`.fw-${i}`] = {
-          fontWeight: i * 100,
-        };
-      }
-      return utilities;
-    });
-  },
-  {
-    variants: {
-      extend: {
-        fontWeight: ['responsive', 'hover', 'focus', 'active', 'group-hover'],
-      },
-    },
-  },
-);
+module.exports = plugin(function ({ addUtilities, theme }) {
+  let utilities = {};
+  for (let i = 1; i <= 9; i++) {
+    utilities[`.fw-${i}`] = {
+      fontWeight: i * 100,
+    };
+  }
+  // Add utilities without specifying 'variants' since it is no longer a valid argument
+  addUtilities(utilities);
+});
