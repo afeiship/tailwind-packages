560ed2535b5502cb1ca3011d5be064d57ca304e8
afeiship <1290657123@qq.com>
2024-11-06 13:31:02 +0800

    feat: add is-x/is-y for flex-scrollable

--- packages/tailwind-layouts/src/modules/layout-flex-scrollable.js
+++ packages/tailwind-layouts/src/modules/layout-flex-scrollable.js
@@ -1,16 +1,24 @@
 module.exports = function (pluginApi) {
   const { addComponents } = pluginApi
   return addComponents({
-    '.layout-flex-scrollable': { display: 'flex', flexDirection: 'column', height: '100%' },
-    '.is-flex-scrollable-y': {
-      flex: 1,
-      overflow: 'hidden scroll',
-      overscrollBehavior: 'contain',
-    },
-    '.is-flex-scrollable-x': {
-      flex: 1,
-      overflow: 'hidden scroll',
-      overscrollBehavior: 'contain',
+    '.layout-flex-scrollable': {
+      display: 'flex',
+      '&.is-x': {
+        flexDirection: 'row',
+      },
+      '&.is-y': {
+        flexDirection: 'column',
+      },
+      '.is-flex-scrollable-y': {
+        flex: 1,
+        overflow: 'hidden scroll',
+        overscrollBehavior: 'contain',
+      },
+      '.is-flex-scrollable-x': {
+        flex: 1,
+        overflow: 'hidden scroll',
+        overscrollBehavior: 'contain',
+      },
     },
   })
 }
