9a836ddf07f8eab6ca5e768a6e5b15b339b40b77
afeiship <1290657123@qq.com>
2024-11-06 11:30:45 +0800

    feat: add layout-flex-scrollable

--- packages/tailwind-layouts/src/index.js
+++ packages/tailwind-layouts/src/index.js
@@ -1,23 +1,25 @@
-const plugin = require('tailwindcss/plugin');
-const moduleLayoutAbsCenter = require('./modules/layout-abs-center');
-const moduleLayoutAbsScrollable = require('./modules/layout-abs-scrollable');
-const moduleLayoutInlineItems = require('./modules/layout-inline-items');
-const moduleLayoutStickyFooter = require('./modules/layout-sticky-footer');
-const moduleTransCenter = require('./modules/layout-trans-center');
-const moduleLayoutTextJustify = require('./modules/layout-text-justify');
-const moduleLayoutFlex = require('./modules/layout-flex');
-const moduleLayoutFlexList = require('./modules/layout-flex-list');
-const moduleLayoutFloatList = require('./modules/layout-float-list');
+const plugin = require('tailwindcss/plugin')
+const moduleLayoutAbsCenter = require('./modules/layout-abs-center')
+const moduleLayoutAbsScrollable = require('./modules/layout-abs-scrollable')
+const moduleLayoutInlineItems = require('./modules/layout-inline-items')
+const moduleLayoutStickyFooter = require('./modules/layout-sticky-footer')
+const moduleTransCenter = require('./modules/layout-trans-center')
+const moduleLayoutTextJustify = require('./modules/layout-text-justify')
+const moduleLayoutFlex = require('./modules/layout-flex')
+const moduleLayoutFlexList = require('./modules/layout-flex-list')
+const moduleLayoutFloatList = require('./modules/layout-float-list')
+const moduleLayoutFlexScrollable = require('./modules/layout-flex-scrollable')
 
-module.exports = plugin(function(pluginApi) {
+module.exports = plugin(function (pluginApi) {
   // Add custom utilities here
-  moduleLayoutAbsCenter(pluginApi);
-  moduleLayoutAbsScrollable(pluginApi);
-  moduleLayoutInlineItems(pluginApi);
-  moduleLayoutStickyFooter(pluginApi);
-  moduleTransCenter(pluginApi);
-  moduleLayoutTextJustify(pluginApi);
-  moduleLayoutFlex(pluginApi);
-  moduleLayoutFlexList(pluginApi);
-  moduleLayoutFloatList(pluginApi);
-});
+  moduleLayoutAbsCenter(pluginApi)
+  moduleLayoutAbsScrollable(pluginApi)
+  moduleLayoutInlineItems(pluginApi)
+  moduleLayoutStickyFooter(pluginApi)
+  moduleTransCenter(pluginApi)
+  moduleLayoutTextJustify(pluginApi)
+  moduleLayoutFlex(pluginApi)
+  moduleLayoutFlexList(pluginApi)
+  moduleLayoutFloatList(pluginApi)
+  moduleLayoutFlexScrollable(pluginApi)
+})
---
+++ packages/tailwind-layouts/src/modules/layout-flex-scrollable.js
@@ -0,0 +1,16 @@
+module.exports = function (pluginApi) {
+  const { addComponents } = pluginApi
+  addComponents({
+    '.layout-flex-scrollable': { display: 'flex', flexDirection: 'column', height: '100%' },
+    '.is-flex-scrollable-y': {
+      flex: 1,
+      overflow: 'hidden scroll',
+      overscrollBehavior: 'contain',
+    },
+    '.is-flex-scrollable-x': {
+      flex: 1,
+      overflow: 'hidden scroll',
+      overscrollBehavior: 'contain',
+    },
+  })
+}
