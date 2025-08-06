011d5226af314b62ae5515ee94c404d5fadd35b7
aric.zheng <1290657123@qq.com>
2025-08-07 07:06:02 +0800

    feat: upt radius trbl zero

--- packages/tailwind-shortcuts/src/modules/radius.js
+++ packages/tailwind-shortcuts/src/modules/radius.js
@@ -16,6 +16,18 @@ module.exports = function (pluginConfig) {
       'border-bottom-left-radius': 0,
       'border-top-left-radius': 0,
     },
+    '.radius-tl0': {
+      'border-top-left-radius': 0,
+    },
+    '.radius-tr0': {
+      'border-top-right-radius': 0,
+    },
+    '.radius-bl0': {
+      'border-bottom-left-radius': 0,
+    },
+    '.radius-br0': {
+      'border-bottom-right-radius': 0,
+    },
     '.radius-inherit': {
       'border-radius': 'inherit',
     },
