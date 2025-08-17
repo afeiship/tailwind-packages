577df0eac292a2728606e757d5ab045953d98bf8
aric.zheng <1290657123@qq.com>
2025-08-17 11:30:37 +0800

    fix: rm debug imp

--- packages/tailwind-shortcuts/src/modules/debug.js
+++ packages/tailwind-shortcuts/src/modules/debug.js
@@ -2,26 +2,26 @@ module.exports = function (pluginConfig) {
   const { addComponents } = pluginConfig;
   return addComponents({
     '.debug': {
-      border: '1px solid #f30 !important',
+      border: '2px solid #f30',
     },
     '.debug-red': {
-      border: '1px solid #f00 !important',
+      border: '2px solid #f00',
     },
     '.debug-green': {
-      border: '1px solid #0f0 !important',
+      border: '2px solid #0f0',
     },
     '.debug-blue': {
-      border: '1px solid #00f !important',
+      border: '2px solid #00f',
     },
     // use outline
     '.debug-outline-red': {
-      outline: '1px solid #f00 !important',
+      outline: '2px solid #f00',
     },
     '.debug-outline-green': {
-      outline: '1px solid #0f0 !important',
+      outline: '2px solid #0f0',
     },
     '.debug-outline-blue': {
-      outline: '1px solid #00f !important',
+      outline: '2px solid #00f',
     },
   });
 };
