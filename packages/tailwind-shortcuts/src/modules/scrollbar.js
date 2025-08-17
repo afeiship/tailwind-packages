e7cb27fb53e0a828a09a773ddf22596bb88e18f2
aric.zheng <1290657123@qq.com>
2025-08-17 11:01:09 +0800

    feat: add wkit w options

--- packages/tailwind-shortcuts/src/modules/scrollbar.js
+++ packages/tailwind-shortcuts/src/modules/scrollbar.js
@@ -1,10 +1,30 @@
+// .wkit-scrollbar-w-1::-webkit-scrollbar { width: 0.25rem; }
+// .wkit-scrollbar-w-4::-webkit-scrollbar { width: 1rem; }
+// .wkit-scrollbar-w-8::-webkit-scrollbar { width: 2rem; }
+
 module.exports = function (pluginConfig) {
-  const { addComponents } = pluginConfig;
-  return addComponents({
+  const { addComponents, matchUtilities, theme } = pluginConfig;
+  addComponents({
     '.wkit-scrollbar-none': {
       '&::-webkit-scrollbar': {
         display: 'none',
-      }
-    }
+      },
+    },
+    '.wkit-scrollbar-block': {
+      '&::-webkit-scrollbar': {
+        display: 'block',
+      },
+    },
   });
+
+  matchUtilities(
+    {
+      'wkit-scrollbar-w': (value) => ({
+        '&::-webkit-scrollbar': {
+          width: value,
+        },
+      }),
+    },
+    { values: theme('spacing') },
+  );
 };
