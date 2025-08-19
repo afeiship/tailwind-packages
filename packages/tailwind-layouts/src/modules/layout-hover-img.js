a9e7382f307ea82865e17705778dc65002ce0018
aric.zheng <1290657123@qq.com>
2025-08-19 16:30:55 +0800

    fix: rong scale value

--- packages/tailwind-layouts/src/modules/layout-hover-img.js
+++ packages/tailwind-layouts/src/modules/layout-hover-img.js
@@ -1,12 +1,3 @@
-const baseStyles = {
-  position: 'absolute',
-  top: 0,
-  left: 0,
-  right: 0,
-  bottom: 0,
-  scrollBehavior: 'smooth',
-}
-
 module.exports = function (pluginApi) {
   const { addComponents } = pluginApi
 
@@ -21,7 +12,7 @@ module.exports = function (pluginApi) {
         transition: 'transform 0.3s ease-in-out',
         willChange: 'transform',
         '&:hover': {
-          transform: 'scale(1.0.5)',
+          transform: 'scale(1.05)',
         },
       },
     },
