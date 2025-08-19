a406e7aaddd989c17a9a82d729c51174acd0e16d
aric.zheng <1290657123@qq.com>
2025-08-19 16:17:40 +0800

    feat: add base styles

--- packages/tailwind-wsui/src/modules/opacity-bg.js
+++ packages/tailwind-wsui/src/modules/opacity-bg.js
@@ -1,16 +1,23 @@
+const baseStyles = {
+  position: 'absolute',
+  color: 'white',
+  borderRadius: '100rem', // 默认圆角,
+  fontSize: '0.75rem',
+  height: '1.25rem',
+  minWidth: '5rem',
+  textAlign: 'center'
+};
+
 module.exports = function (pluginConfig) {
-  const { matchComponents } = pluginConfig;
+  const { matchComponents, addBase } = pluginConfig;
+  addBase({ '[class*="layout-abs-center"]': { ...baseStyles } });
+
   // wsui-opacity-bg-40
   matchComponents({
     'wsui-opacity-bg': (value) => {
       const alpha = parseFloat(value / 100);
       return {
-        backgroundColor: `rgba(0, 0, 0, ${alpha})`,
-        borderRadius: '100rem', // 默认圆角,
-        fontSize: '0.75rem',
-        height: '1.25rem',
-        minWidth: '5rem',
-        textAlign: 'center'
+        backgroundColor: `rgba(0, 0, 0, ${alpha})`
       };
     }
   });
