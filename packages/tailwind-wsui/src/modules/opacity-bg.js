d6b247d7a380b0b42706967999d41fa925332514
aric.zheng <1290657123@qq.com>
2025-08-19 16:14:42 +0800

    fix: upt usage

--- packages/tailwind-wsui/src/modules/opacity-bg.js
+++ packages/tailwind-wsui/src/modules/opacity-bg.js
@@ -8,8 +8,8 @@ module.exports = function (pluginConfig) {
         backgroundColor: `rgba(0, 0, 0, ${alpha})`,
         borderRadius: '100rem', // 默认圆角,
         fontSize: '0.75rem',
-        height: '4rem',
-        minWidth: '20rem',
+        height: '1.25rem',
+        minWidth: '5rem',
         textAlign: 'center'
       };
     }
