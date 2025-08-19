14f437db3e95a243419b664e8237de3d402c7b16
aric.zheng <1290657123@qq.com>
2025-08-19 16:04:44 +0800

    feat: add opacity-bg

--- packages/tailwind-wsui/src/modules/opacity-bg.js
+++ packages/tailwind-wsui/src/modules/opacity-bg.js
@@ -1,12 +1,17 @@
 module.exports = function (pluginConfig) {
-  const { matchUtilities } = pluginConfig;
-  // wsui-opacity-bg-*
-  matchUtilities(
-    {
-      'wsui-opacity-bg': (value) => ({
-        backgroundColor: value
-      })
-    },
-    { values: theme('opacity') }
-  );
+  const { matchComponents } = pluginConfig;
+  // wsui-opacity-bg-40
+  matchComponents({
+    'wsui-opacity-bg': (value) => {
+      const alpha = parseFloat(value / 100);
+      return {
+        backgroundColor: `${rgba(0, 0, 0, alpha)}`,
+        borderRadius: '100rem', // 默认圆角,
+        fontSize: '0.75rem',
+        height: '4rem',
+        minWidth: '20rem',
+        textAlign: 'center'
+      };
+    }
+  });
 };
