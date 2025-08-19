7f769cbe0bd1b136b7d876710642ae7fa222d464
aric.zheng <1290657123@qq.com>
2025-08-19 16:10:57 +0800

    fix: sytax error

--- packages/tailwind-wsui/src/modules/opacity-bg.js
+++ packages/tailwind-wsui/src/modules/opacity-bg.js
@@ -5,7 +5,7 @@ module.exports = function (pluginConfig) {
     'wsui-opacity-bg': (value) => {
       const alpha = parseFloat(value / 100);
       return {
-        backgroundColor: `${rgba(0, 0, 0, alpha)}`,
+        backgroundColor: `rgba(0, 0, 0, ${alpha})`,
         borderRadius: '100rem', // 默认圆角,
         fontSize: '0.75rem',
         height: '4rem',
