6cdddfb2e2d52675f03dcc9de7452d1e1fee0eee
aric.zheng <1290657123@qq.com>
2025-08-23 18:37:24 +0800

    feat: add bgi for shortcuts

--- packages/tailwind-shortcuts/src/index.js
+++ packages/tailwind-shortcuts/src/index.js
@@ -1,33 +1,35 @@
 // ref: https://github.com/afeiship/wsui-shorty
-const plugin = require('tailwindcss/plugin')
-const moduleBoxSizing = require('./modules/box-sizing')
-const moduleCorner = require('./modules/corner')
-const moduleDebug = require('./modules/debug')
-const moduleFlex = require('./modules/flex')
-const moduleFlexGap = require('./modules/flex-gap')
-const moduleFlip = require('./modules/flip')
-const moduleMargin = require('./modules/margin')
-const modulePadding = require('./modules/padding')
-const modulePosition = require('./modules/position')
-const moduleRadius = require('./modules/radius')
-const moduleScrollbar = require('./modules/scrollbar')
-const moduleText = require('./modules/text')
-const moduleTransform = require('./modules/transform')
-const moduleZIndex = require('./modules/z-index')
+const plugin = require('tailwindcss/plugin');
+const moduleBgi = require('./modules/bgi');
+const moduleBoxSizing = require('./modules/box-sizing');
+const moduleCorner = require('./modules/corner');
+const moduleDebug = require('./modules/debug');
+const moduleFlex = require('./modules/flex');
+const moduleFlexGap = require('./modules/flex-gap');
+const moduleFlip = require('./modules/flip');
+const moduleMargin = require('./modules/margin');
+const modulePadding = require('./modules/padding');
+const modulePosition = require('./modules/position');
+const moduleRadius = require('./modules/radius');
+const moduleScrollbar = require('./modules/scrollbar');
+const moduleText = require('./modules/text');
+const moduleTransform = require('./modules/transform');
+const moduleZIndex = require('./modules/z-index');
 
 module.exports = plugin(function (pluginConfig) {
-  moduleBoxSizing(pluginConfig)
-  moduleCorner(pluginConfig)
-  moduleDebug(pluginConfig)
-  moduleFlex(pluginConfig)
-  moduleFlexGap(pluginConfig)
-  moduleFlip(pluginConfig)
-  moduleMargin(pluginConfig)
-  modulePadding(pluginConfig)
-  modulePosition(pluginConfig)
-  moduleRadius(pluginConfig)
-  moduleScrollbar(pluginConfig)
-  moduleText(pluginConfig)
-  moduleTransform(pluginConfig)
-  moduleZIndex(pluginConfig)
-})
+  moduleBgi(pluginConfig);
+  moduleBoxSizing(pluginConfig);
+  moduleCorner(pluginConfig);
+  moduleDebug(pluginConfig);
+  moduleFlex(pluginConfig);
+  moduleFlexGap(pluginConfig);
+  moduleFlip(pluginConfig);
+  moduleMargin(pluginConfig);
+  modulePadding(pluginConfig);
+  modulePosition(pluginConfig);
+  moduleRadius(pluginConfig);
+  moduleScrollbar(pluginConfig);
+  moduleText(pluginConfig);
+  moduleTransform(pluginConfig);
+  moduleZIndex(pluginConfig);
+});
---
+++ packages/tailwind-shortcuts/src/modules/bgi.js
@@ -0,0 +1,32 @@
+const baseStyles = {
+  'background-size': 'cover',
+  'background-repeat': 'no-repeat',
+  'background-position': 'center',
+};
+
+module.exports = function (pluginConfig) {
+  const { matchUtilities, theme } = pluginConfig;
+  const basePath = theme('backgroundImage.basePath', '/public/images'); // 可配置根路径
+  // 公共函数：生成 URL
+  const buildUrl = (path) => {
+    return `${basePath}/${path}`.replace(/\/+/g, '/');
+  };
+
+  // 公共函数：生成 background-image 规则
+  const bgImageRule = (value) => {
+    const url = buildUrl(value);
+    return { 'background-image': `url('${url}')` };
+  };
+
+  matchUtilities({
+    bgib: (value) => {
+      return {
+        ...bgImageRule(value),
+        ...baseStyles,
+      };
+    },
+    bgi: (value) => {
+      return bgImageRule(value);
+    },
+  });
+};
